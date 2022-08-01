be685d15090af23ee1f211071f7f5792a70bffd7
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-01 16:42:44 +0530

    first commit

---
+++ src/app/subreddit/subreddit.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { SubredditModel } from "./subreddit-response";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SubredditService {
+  constructor(private http: HttpClient) {}
+
+  getAllSubreddits(): Observable<Array<SubredditModel>> {
+    return this.http.get<Array<SubredditModel>>(
+      "https://reddit-clone-sb.herokuapp.com/api/subreddit"
+    );
+  }
+
+  createSubreddit(subredditModel: SubredditModel): Observable<SubredditModel> {
+    return this.http.post<SubredditModel>(
+      "https://reddit-clone-sb.herokuapp.com/api/subreddit",
+      subredditModel
+    );
+  }
+}
