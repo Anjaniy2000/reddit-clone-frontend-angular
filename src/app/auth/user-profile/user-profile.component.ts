f7d9cab6f8fd45c8d5e580cfef6bed827368e955
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-02 19:31:21 +0530

    final refactoring(design)
    final refactoring(design).

--- src/app/auth/user-profile/user-profile.component.ts
+++ src/app/auth/user-profile/user-profile.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from 'src/app/shared/post.service';
-import { ActivatedRoute } from '@angular/router';
-import { CommentService } from 'src/app/comment/comment.service';
-import { PostModel } from 'src/app/shared/post-model';
-import { CommentPayload } from 'src/app/comment/comment.payload';
+import { Component, OnInit } from "@angular/core";
+import { PostService } from "src/app/shared/post.service";
+import { ActivatedRoute } from "@angular/router";
+import { CommentService } from "src/app/comment/comment.service";
+import { PostModel } from "src/app/shared/post-model";
+import { CommentPayload } from "src/app/comment/comment.payload";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.css"],
 })
 export class UserProfileComponent implements OnInit {
   name: string;
@@ -17,21 +17,23 @@ export class UserProfileComponent implements OnInit {
   postLength: number;
   commentLength: number;
 
-  constructor(private activatedRoute: ActivatedRoute, private postService: PostService,
-    private commentService: CommentService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private postService: PostService,
+    private commentService: CommentService
+  ) {
     this.name = this.activatedRoute.snapshot.params.name;
 
-    this.postService.getAllPostsByUser(this.name).subscribe(data => {
+    this.postService.getAllPostsByUser(this.name).subscribe((data) => {
       this.posts = data;
       this.postLength = data.length;
     });
-    this.commentService.getAllCommentsByUser(this.name).subscribe(data => {
+    this.commentService.getAllCommentsByUser(this.name).subscribe((data) => {
       this.comments = data;
+      this.comments = this.comments.reverse();
       this.commentLength = data.length;
     });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
